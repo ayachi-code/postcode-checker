8b3cf075f999cf26e29a33ef7dfbedeacdc92117
bilal <bilal.ayachi@gmail.com>
2017-12-12 12:39:12 +0100

    API key veranderd

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const TeleBot = require('telebot');
 
 const bot = new TeleBot({
-    token: '509335339:AAE2N-6q7wKitgjVX6RhBkF1sBzQUHpkOw0'
+    token: '483010976:AAGGBCf0K8w6zRSmX8_1vHnB4vvdsh171hA'
 });
 
 
